e3e90074e16aea004b2e6fa01080a5892a776867
papgergo1997 <pap.gergo.1997@gmail.com>
2021-02-21 14:42:58 +0100

    toasters work fine

--- ang-basic-practice008-forms/src/app/page/events-list/events-list.component.ts
+++ ang-basic-practice008-forms/src/app/page/events-list/events-list.component.ts
@@ -22,7 +22,7 @@ export class EventsListComponent implements OnInit {
 
   onDelete(event: Event): void {
     this.eventService.remove(event).subscribe();
-    this.toaster.info('You have successfully deleted an event')
+    this.toaster.warning('You have successfully deleted an event')
     this.eventList = this.eventService.getAll();
   }
 
